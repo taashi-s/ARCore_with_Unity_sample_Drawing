6addd3fc0f47987ef5055c4ae201b437c5653776
taashi-s <x7286x_ayk@yahoo.co.jp>
2021-01-29 18:34:40 +0900

    init

---
+++ Assets/Draw.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using GoogleARCore;
+
+public class Draw : MonoBehaviour
+{
+    public GameObject obj;
+    GameObject drawObj;
+
+    // Start is called before the first frame update
+    void Start() {
+        
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if ( Input.touchCount == 1 ) {
+            Vector3 p = Camera.main.transform.TransformPoint(0, 0, 0.1f);
+
+            if ( Input.GetTouch(0).phase == TouchPhase.Began ) {
+                drawObj = GameObject.Instantiate(obj, p, Quaternion.identity);
+            }
+        else if ( Input.GetTouch(0).phase == TouchPhase.Stationary ) {
+            drawObj.transform.position = p;
+        }
+        }
+    }
+}
---
+++ Assets/PutByTap.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using GoogleARCore;
+
+public class PutByTap : MonoBehaviour
+{
+    public GameObject pown;
+    // Start is called before the first frame update
+    void Start() {
+        
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if ( Input.touchCount < 1 ) {
+            return;
+        }
+        Touch touch = Input.GetTouch (0);
+
+        if ( touch.phase != TouchPhase.Moved ) {
+            return;
+        }
+
+        TrackableHit hit;
+        TrackableHitFlags filter = TrackableHitFlags.PlaneWithinPolygon;
+        if (Frame.Raycast(touch.position.x, touch.position.y, filter, out hit)) {
+            if (hit.Trackable is DetectedPlane){
+                pown.transform.position = hit.Pose.position;
+                pown.transform.rotation = hit.Pose.rotation;
+                pown.transform.Rotate(0, 180, 0, Space.Self);
+
+                var anchor = hit.Trackable.CreateAnchor(hit.Pose);
+                pown.transform.parent = anchor.transform;
+            }
+        }
+    }
+}
